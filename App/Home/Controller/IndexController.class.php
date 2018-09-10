32845f06307b6617366fbe6320a008a50b518089
Zhaokaihui <254665468@qq.com>
2018-09-10 15:45:37 +0800

    回滚

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -47,7 +47,7 @@ class IndexController extends Controller{
  	    $Photo = D('photo');
  	    $photo_list = $Photo
  	    ->join('photo_album_relation ON photo_album_relation.photo_id = photo.id')
- 	    ->where("photo_album_relation.album_id='$album_id' and photo.is_delete = 0")
+ 	    ->where("photo_album_relation.album_id='%d'",$album_id)
  	    ->order(array('photo.sort'=>'desc','photo.id'=>'asc'))
  	    ->select();
  	
@@ -59,6 +59,45 @@ class IndexController extends Controller{
  	        }
  	    }
  	    $this->assign('photo_list',$photo_list);
+ 	    $this->display();
+ 	}
+ 	
+ 	/*
+ 	 * 首页相册列表
+ 	 */
+ 	public function album_show(){
+ 	    $Album = D('album');
+ 	    $Theme = D('theme');
+ 	    $Relation = D('album_theme_relation');
+ 	    $theme_list = $Theme->where('is_delete=%d',0)->select();
+ 	    
+ 	    $album_list = $Album
+ 	    ->field('album.*')
+ 	    ->join('left join photo_album_relation ON photo_album_relation.album_id = album.id')
+ 	    ->where("album.is_delete=0 and photo_album_relation.album_id <> ''")
+ 	    ->order(array('album.sort'=>'desc','album.id'=>'asc'))
+ 	    ->group('album.id')
+ 	    ->select();
+
+ 	    //获取相册对应的主题id
+ 	    if(!empty($album_list) && is_array($album_list)){
+ 	        foreach ($album_list as $key => $val){
+ 	            $album_list[$key]['theme_ids'] = '';
+ 	            $album_from_theme = $Relation
+ 	            ->field('theme_name')
+ 	            ->join('theme ON theme.id = album_theme_relation.theme_id')
+ 	            ->where("album_theme_relation.album_id='%d'",$val['id'])
+ 	            ->select();
+ 	            if(!empty($album_from_theme)){
+ 	                foreach ($album_from_theme as $k => $v){
+ 	                    $album_list[$key]['theme_names'] .=$v['theme_name'].',';
+ 	                }
+ 	                $album_list[$key]['theme_names'] = trim($album_list[$key]['theme_names'],',');
+ 	            }
+ 	        }
+ 	    }
+ 	    $this->assign('theme_list',$theme_list);
+ 	    $this->assign('album_list',$album_list);
  	    $this->display();
  	}
 }
