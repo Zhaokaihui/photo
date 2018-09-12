66b234862e969e48d84e8bca7c6c244249652f71
Zhaokaihui <254665468@qq.com>
2018-09-12 16:18:52 +0800

    index

--- App/Home/Model/IndexModel.class.php
+++ App/Home/Model/IndexModel.class.php
@@ -11,31 +11,9 @@
 			return count($list);
 		}
 
-		function judge($email){
-			$Model=D('User');
-			$map['Email']=$email;
-			$judge=$Model->where($map)->select();
-			return $judge[0];
-		}
-
-		function doSign($email,$pwd,$role){
-			/*数据库存储*/
-			$judge=$this->judge($email);
-			if($judge){
-				return false;
-			}else{
-				$data['User_name']=$email;
-				$data['Email']=$email;
-				$data['Pwd']=$pwd;
-				$data['Face']='default.jpg';
-				$data['Status']=0;
-				$data['Role']=$role;
-				$Model=D('User');
-				$result=$Model->data($data)->add();
-				return $result;
-			}
-		}
-		
+		/*
+		 * 获取首页相册列表
+		 */
 		function getIndexAlbumList(){
 		    $Album = D('album');
 		    $list = $Album
@@ -48,6 +26,14 @@
 
 		    return $list;
 		}
+		
+		/**
+		 * 获取首页主题列表
+		 */
+		function getIndexThemeList(){
+		    
+		}
+		
 	}
 
 ?>
